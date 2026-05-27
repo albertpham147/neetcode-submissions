524906bf7dee83a37ca8f2f2450d2dd8711db0ef
albertpham147 <albertpham451@gmail.com>
2026-05-27 19:36:42 +0700

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.cs
@@ -0,0 +1,37 @@
+public class Solution {
+
+    public string Encode(IList<string> strs)
+    {
+        string encodeStr = "";
+
+        if(strs.Count == 0)
+        {
+            return null;
+        }
+
+        for (int i = 0; i < strs.Count; i++)
+        {
+            encodeStr += strs[i];
+            if(i >= 0 && i < strs.Count - 1)
+            {
+                encodeStr += "`";
+            }
+        }
+
+        return encodeStr;
+    }
+
+    public List<string> Decode(string s)
+    {
+        if(s == null)
+        {
+            return new List<string>() { };
+        }
+
+        List<string> result = new List<string>();
+
+        result = s.Split("`").ToList();
+
+        return result;
+    }
+}
