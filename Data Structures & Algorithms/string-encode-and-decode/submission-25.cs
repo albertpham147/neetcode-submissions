17e67d27217a6cfa5577557932f07cd2f911548f
albertpham147 <albertpham451@gmail.com>
2026-05-28 14:54:34 +0700

    Add: string-encode-and-decode - submission-25

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-25.cs
@@ -0,0 +1,64 @@
+public class Solution {
+
+    public string Encode(IList<string> strs)
+    {
+        string encodeStr = null;
+
+        if(strs.Count == 0)
+        {
+            return encodeStr;
+        }
+
+        for (int i = 0; i < strs.Count; i++)
+        {
+            encodeStr += strs[i];
+            if(i >= 0 && i < strs.Count - 1)
+            {
+                encodeStr += "`";
+            }
+        }
+
+        return encodeStr;
+    }
+
+    public List<string> Decode(string s)
+    {
+        if(s is null) // check null
+        {
+            Console.WriteLine("Decode check null");
+            return new List<string>() { };
+        }
+        if (string.IsNullOrEmpty(s)) // check empty
+        {
+            return new List<string>() { "" };
+        }
+
+        List<string> result = new List<string>();
+
+
+        string str = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            bool canAdd = true;
+
+            if (s[i] == '`')
+            {
+                result.Add(str);
+                str = "";
+                canAdd = false;
+            }
+
+            if(canAdd)
+                str += s[i];
+
+
+            if (i == s.Length - 1)
+            {
+                result.Add(str);
+                break;
+            }
+        }
+
+        return result;
+    }
+}
